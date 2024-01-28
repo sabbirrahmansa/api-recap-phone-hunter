24f962b5fcffb593628250272158462814aca898
Md.Sabbir Rahman Saun <sabbirrahman3586@gmail.com>
2024-01-28 22:06:38 +0600

    modifi

--- js/phone.js
+++ js/phone.js
@@ -1,6 +1,9 @@
 
-const loadphone = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+const loadphone = async (searchText) => {
+    // modifi url , in paramiter
+    const res = await fetch(` 
+    https://openapi.programming-hero.com/api/phones?search=${searchText}
+    `)
     const data =  await res.json();
     const phones = data.data;
     // console.log(phone)
@@ -9,10 +12,11 @@ const loadphone = async () => {
 
 }
 
-const displayPhone = phones =>{
+const displayPhone = (phones) =>{
     // console.log(phones)
     // 1 step get element by id
     const phoneContainer = document.getElementById('phone-container')
+    phoneContainer.textContent ='';
 
    phones.forEach(phone =>{
 
@@ -20,14 +24,15 @@ const displayPhone = phones =>{
 
      // 2 step creat a div
      const phoneCard = document.createElement('div')
-     phoneCard.classList=`card w-96 bg-gray-100 shadow-xl`;
+     phoneCard.classList=`card  bg-gray-500 p-4 shadow-xl`;
 
      // 3 step inner html
      phoneCard.innerHTML =`
      <figure><img src="${phone.image}" alt="Shoes" /></figure>
      <div class="card-body">
          <h2 class="card-title">${phone. phone_name}</h2>
-         <p>If a dog chews shoes whose shoes does he choose?</p>
+         <h5> brand ${phone. brand}</h5>
+         <p>${phone.slug}</p>
          <div class="card-actions justify-end">
          <button class="btn btn-primary">Buy Now</button>
          </div>
@@ -47,8 +52,20 @@ const displayPhone = phones =>{
 
 
 }
+// button a onclick deya function ta bosaiya deshi
+const hendelSearch = () =>{
+    // 2 line korsi text field ar id neya value bosaisi , ar tar thaky velue neshi
+    const searchField = document.getElementById('input-Field')
+    const searchText = searchField.value ;
+
+    // loadphone function ta call koray deshi, and paramiter bosaisi
+    loadphone(searchText)
+    
+}
 
 
 
 
-loadphone() ;
+
+
+ 
